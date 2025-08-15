d0e134d6f7855833617044003347b060c60d689d
Arthur Azevedo <arthur.makets@gmail.com>
2025-08-15 00:25:50 +0000

    page+profile

---
+++ ex01-next/app/components/Profile.js
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      width="150"
+      height="150"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
