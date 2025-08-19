0ed3c52aa7e72b98bd2ab3f0431ccfe68d7df5f5
Arthur Azevedo <arthur.makets@gmail.com>
2025-08-19 00:37:15 +0000

    aula 18-08

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,16 +1,20 @@
 //import Image from "next/image";
 //import styles from "./page.module.css";
 
-import { Profile } from "./components/Profile";
+import { Profile } from "../components/Profile";
+import { TodoList } from "../components/TodoList";
 
 export default function Home() {
   return (
-    <div>
+    <section>
       <h1>Amazing scientists</h1>
       <br />
       <Profile />
       <Profile />
       <Profile />
-    </div>
+      <TodoList />
+    </section>
+    
+    
   );
 }
