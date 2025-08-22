291fc83a15cb39ccea948c8bc8515dd2ff853234
Arthur Azevedo <arthur.makets@gmail.com>
2025-08-22 00:34:41 +0000

    ajeitando uns negócio aí

--- atv-react/components/TodoList.js
+++ atv-react/components/TodoList.js
@@ -15,7 +15,7 @@ export function TodoList() {
           <h1>ARTHUR AZEVEDO COSTA DE PAULA</h1>
           <br></br>
             <p>
-              "Estudante de programação. Comecei aprendendo a lógica com Java, onde trabalhei com exercícios para organizar informações, resolver problemas matemáticos e usar repetições para criar padrões. Agora, estou no processo de aprender a criar sites e componentes usando React e Next.js. Já consegui configurar meu primeiro projeto e construir meu primeiro componente. Tenho me esforçado para entender os conceitos por trás do código e resolver os desafios que aparecem. Estou no começo da minha jornada, e animado para continuar construindo minhas habilidades como desenvolvedor web."
+              &#34 Estudante de programação. Comecei aprendendo a lógica com Java, onde trabalhei com exercícios para organizar informações, resolver problemas matemáticos e usar repetições para criar padrões. Agora, estou no processo de aprender a criar sites e componentes usando React e Next.js. Já consegui configurar meu primeiro projeto e construir meu primeiro componente. Tenho me esforçado para entender os conceitos por trás do código e resolver os desafios que aparecem. Estou no começo da minha jornada, e animado para continuar construindo minhas habilidades como desenvolvedor web. &#34
             </p>
             <br></br>
             <p>Minhas tarefas para hoje:</p>
